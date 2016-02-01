26317085928a488e2925e5ea7c0c94e3096da968
Amine Lajmi <amine@plugbee.com>
2016-02-01 23:44:42 +0100

    moved model creation wizards to xtext common

---
+++ plugins/org.dslforge.xtext.common/src/org/dslforge/xtext/common/actions/NewModelAction.java
@@ -0,0 +1,41 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2015 PlugBee. All rights reserved.
+ * 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Eclipse Public License v1.0 which 
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Amine Lajmi - Initial API and implementation
+ *
+ * </copyright>
+ */
+package org.dslforge.xtext.common.actions;
+
+import java.io.File;
+
+import org.dslforge.workspace.ui.actions.AbstractWorkspaceAction;
+import org.dslforge.xtext.common.wizards.NewModelFileWizard;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+
+public class NewModelAction extends AbstractWorkspaceAction {
+	
+	@Override
+	public void run(IAction action) {
+		ISelection selection = getSelection();
+		if (!selection.isEmpty()) {
+			Object firstElement = ((IStructuredSelection)selection).getFirstElement();
+			NewModelFileWizard wizard = new NewModelFileWizard((File)firstElement); 
+			wizard.init(getWindow().getWorkbench(), (IStructuredSelection)selection);
+			WizardDialog wizardDialog = new WizardDialog(getWindow().getShell(), wizard);
+			wizardDialog.setPageSize(400, 400);
+			wizardDialog.open();
+		}
+	}
+}
