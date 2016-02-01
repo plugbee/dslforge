999c564cc069e5e9fc0aec52f6f58ec1d289d22d
Amine Lajmi <amine@plugbee.com>
2016-02-01 23:59:34 +0100

    added action getter in text editor contributor

--- plugins/org.dslforge.texteditor/src/org/dslforge/texteditor/BasicTextEditorContributor.java
+++ plugins/org.dslforge.texteditor/src/org/dslforge/texteditor/BasicTextEditorContributor.java
@@ -24,6 +24,7 @@ import org.dslforge.texteditor.actions.DeleteAction;
 import org.dslforge.texteditor.actions.PasteAction;
 import org.dslforge.texteditor.actions.RedoAction;
 import org.dslforge.texteditor.actions.UndoAction;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.ui.IActionBars;
@@ -189,6 +190,31 @@ public class BasicTextEditorContributor extends EditorActionBarContributor imple
 		undoAction.update();
 		redoAction.update();
 
+	}
+	
+	/**
+	 * Returns the action registered with the given text editor.
+	 *
+	 * @param editor the editor, or <code>null</code>
+	 * @param actionId the action id
+	 * @return the action, or <code>null</code> if none
+	 */
+	protected final IAction getAction(BasicTextEditor editor, String actionId) {
+		return (editor == null || actionId == null ? null : getAction(actionId));
+	}
+
+	private IAction getAction(String actionId) {
+		if (actionId.equals(ActionFactory.COPY.getId()))
+			return copyAction;
+		if (actionId.equals(ActionFactory.CUT.getId()))
+			return cutAction;
+		if (actionId.equals(ActionFactory.PASTE.getId()))
+			return pasteAction;
+		if (actionId.equals(ActionFactory.UNDO.getId()))
+			return undoAction;
+		if (actionId.equals(ActionFactory.REDO.getId()))
+			return redoAction;
+		return null;
 	}
 
 	@Override
