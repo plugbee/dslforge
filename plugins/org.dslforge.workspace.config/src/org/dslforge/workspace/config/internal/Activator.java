9de37354a805e434dae2b642886a961ee154df63
Amine Lajmi <amine@plugbee.com>
2016-08-03 01:12:23 +0200

    extracted (and renamed) workspace configuration extension point into .config plugin

---
+++ plugins/org.dslforge.workspace.config/src/org/dslforge/workspace/config/internal/Activator.java
@@ -0,0 +1,33 @@
+package org.dslforge.workspace.config.internal;
+
+import org.apache.log4j.Logger;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	static final Logger logger = Logger.getLogger(Activator.class);
+	
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.dslforge.workspace.config"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		plugin = this;
+		logger.info(PLUGIN_ID + " started!");
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		logger.info(PLUGIN_ID + " stopping!");
+		plugin = null;
+	}
+
+}
