9d416a9b0bd86ae3dc728860dc3a24ae82305cea
Amine Lajmi <amine@plugbee.com>
2016-11-29 15:25:34 +0100

    removed duplicate domain model example

--- examples/org.xtext.example.domainmodel.web/src-js/org/xtext/example/domainmodel/web/ace/snippets/domainmodel.js
+++
@@ -1,9 +0,0 @@
-define('ace/snippets/domainmodel', ['require', 'exports', 'module' ], function(require, exports, module) {
-
-exports.snippetText = '\
-# EmptyEntity\nsnippet Empty Entity\n	entity ${1:name} { \n		${2://body}\n	}\n\
-# EntityWithAttribute\nsnippet Entity With Attribute\n	entity ${1:name} { \n		${2://Attributes}\n		${3:name} : ${4:type}\n	}\n\
-# EntityWithFunction\nsnippet Entity With Function\n	entity ${1:name} { \n		${2://Functions}\n		op ${3:name}(${4:param_x}, ${5:param_y}) : ${4:return_type}\n	}\n\
-'
-exports.scope = "domainmodel";
-});
