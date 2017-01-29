02f560ec32359c498f802a57acbc4f15e002d06f
Amine Lajmi <amine@plugbee.com>
2017-01-29 21:51:19 +0100

    enhanced editor extamples themes

--- plugins/org.dslforge.styledtext/src-js/org/dslforge/styledtext/ace/theme-basic.js
+++ plugins/org.dslforge.styledtext/src-js/org/dslforge/styledtext/ace/theme-basic.js
@@ -1,38 +1,22 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Distributed under the BSD license:
+/**
+ * <copyright>
  *
- * Copyright (c) 2010, Ajax.org B.V.
- * All rights reserved.
+ * Copyright (c) 2016 PlugBee. All rights reserved.
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Ajax.org B.V. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL AJAX.ORG B.V. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * This program and the accompanying materials are made available 
+ * under the terms of the basictext Public License v1.0 which 
+ * accompanies this distribution, and is available at
+ * http://www.basictext.org/legal/epl-v10.html
  *
- * ***** END LICENSE BLOCK ***** */
-
-define('ace/theme/eclipse', ['require', 'exports', 'module' , 'ace/lib/dom'], function(require, exports, module) {
-
+ * Contributors:
+ *     PlugBee - Initial API and implementation
+ *
+ * </copyright>
+ */
+define('ace/theme/basic', ['require', 'exports', 'module' , 'ace/lib/dom'], function(require, exports, module) {
 
 exports.isDark = false;
-exports.cssText = ".ace-eclipse .ace_gutter {\
+exports.cssText = ".ace-basic .ace_gutter {\
 background: #ebebeb;\
 border-right: 1px solid rgb(159, 159, 159);\
 color: rgb(136, 136, 136);\
@@ -74,90 +58,86 @@ font-size: 14px;\
 letter-spacing: normal;\
 pointer-events: none;\
 }\
-.ace-eclipse .ace_print-margin {\
+.ace-basic .ace_print-margin {\
 width: 1px;\
 background: #ebebeb;\
 }\
-.ace-eclipse {\
+.ace-basic {\
 background-color: #FFFFFF;\
 }\
-.ace-eclipse .ace_fold {\
+.ace-basic .ace_fold {\
 background-color: rgb(60, 76, 114);\
 }\
-.ace-eclipse .ace_cursor {\
+.ace-basic .ace_cursor {\
 border-left: 2px solid black;\
 }\
-.ace-eclipse .ace_storage,\
-.ace-eclipse .ace_keyword,\
-.ace-eclipse .ace_variable {\
+.ace-basic .ace_storage,\
+.ace-basic .ace_keyword,\
+.ace-basic .ace_variable {\
 font-weight: bold;\
 color: rgb(120, 0, 45);\
 }\
-.ace-eclipse .ace_constant.ace_buildin {\
+.ace-basic .ace_constant.ace_buildin {\
 color: rgb(88, 72, 246);\
 }\
-.ace-eclipse .ace_constant.ace_library {\
+.ace-basic .ace_constant.ace_library {\
 color: rgb(6, 150, 14);\
 }\
-.ace-eclipse .ace_function {\
+.ace-basic .ace_function {\
 color: rgb(60, 76, 114);\
 }\
-.ace-eclipse .ace_string {\
+.ace-basic .ace_string {\
 color: rgb(42, 0, 255);\
 }\
-.ace-eclipse .ace_comment {\
+.ace-basic .ace_comment {\
 color: rgb(113, 150, 130);\
 }\
-.ace-eclipse .ace_comment.ace_doc {\
+.ace-basic .ace_comment.ace_doc {\
 color: rgb(63, 95, 191);\
 }\
-.ace-eclipse .ace_comment.ace_doc.ace_tag {\
+.ace-basic .ace_comment.ace_doc.ace_tag {\
 color: rgb(127, 159, 191);\
 }\
-.ace-eclipse .ace_constant.ace_numeric {\
+.ace-basic .ace_constant.ace_numeric {\
 color: darkblue;\
 }\
-.ace-eclipse .ace_tag {\
+.ace-basic .ace_tag {\
 color: rgb(25, 118, 116);\
 }\
-.ace-eclipse .ace_type {\
+.ace-basic .ace_type {\
 color: rgb(127, 0, 127);\
 }\
-.ace-eclipse .ace_xml-pe {\
+.ace-basic .ace_xml-pe {\
 color: rgb(104, 104, 91);\
 }\
-.ace-eclipse .ace_marker-layer .ace_selection {\
+.ace-basic .ace_marker-layer .ace_selection {\
 background: rgb(181, 213, 255);\
 }\
-.ace-eclipse .ace_marker-layer .ace_bracket {\
+.ace-basic .ace_marker-layer .ace_bracket {\
 margin: -1px 0 0 -1px;\
 border: 1px solid rgb(192, 192, 192);\
 }\
-.ace-eclipse .ace_meta.ace_tag {\
+.ace-basic .ace_meta.ace_tag {\
 color:rgb(25, 118, 116);\
 }\
-.ace-eclipse .ace_invisible {\
+.ace-basic .ace_invisible {\
 color: #ddd;\
 }\
-.ace-eclipse .ace_entity.ace_other.ace_attribute-name {\
+.ace-basic .ace_entity.ace_other.ace_attribute-name {\
 color:rgb(127, 0, 127);\
 }\
-.ace-eclipse .ace_marker-layer .ace_step {\
+.ace-basic .ace_marker-layer .ace_step {\
 background: rgb(255, 255, 0);\
 }\
-.ace-eclipse .ace_marker-layer .ace_active-line {\
+.ace-basic .ace_marker-layer .ace_active-line {\
 background: rgb(232, 242, 254);\
 }\
-.ace-eclipse .ace_marker-layer .ace_selected-word {\
+.ace-basic .ace_marker-layer .ace_selected-word {\
 border: 1px solid rgb(181, 213, 255);\
 }\
-.ace-eclipse .ace_indent-guide {\
+.ace-basic .ace_indent-guide {\
 background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4////f4bLly//BwAmVgd1/w11/gAAAABJRU5ErkJggg==\") right repeat-y;\
 }\
-.ace-tooltip { border: 1px solid silver; border-radius: 3px; color: #444; padding: 2px 5px; padding-right:15px; /*for close button*/ font-size: 90%; font-family: monospace; background-color: white; white-space: pre-wrap; max-width: 50em; max-height:30em; overflow-y:auto; position: absolute; z-index: 10; -webkit-box-shadow: 2px 3px 5px rgba(0, 0, 0, .2); -moz-box-shadow: 2px 3px 5px rgba(0, 0, 0, .2); box-shadow: 2px 3px 5px rgba(0, 0, 0, .2); transition: opacity 1s; -moz-transition: opacity 1s; -webkit-transition: opacity 1s; -o-transition: opacity 1s; -ms-transition: opacity 1s; }\
-.ace-tooltip-boxclose { position:absolute; top:0; right:3px; color:red; }\
-.ace-tooltip-boxclose:hover { background-color:yellow; }\
-.ace-tooltip-boxclose:before { content:'×'; cursor:pointer; font-weight:bold; font-size:larger; }\
 .ace-completion { padding-left: 12px; position: relative; }\
 .ace-completion:before { position: absolute; left: 0; bottom: 0; border-radius: 50%; font-weight: bold; height: 13px; width: 13px; font-size:11px; /*BYM*/ line-height: 14px; text-align: center; color: white; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; }\
 .ace-completion-unknown:before { content:'?'; background: #4bb; }\
@@ -168,20 +148,10 @@ background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZ
 .ace-completion-number:before { content:'1'; background: #999; }\
 .ace-completion-string:before { content:'S'; background: #999; }\
 .ace-completion-bool:before { content:'B'; background: #999; }\
-.ace_autocomplete {\
-    width: 320px;\
-    z-index: 200000;\
-    background: #ffffff;\
-    color: #444;\
-    border: 2px lightgray solid;\
-    position: fixed;\
-    box-shadow: 2px 3px 5px rgba(0,0,0,.2);\
-    line-height: 1.4;\
-}\
 ";
 
 
-exports.cssClass = "ace-eclipse";
+exports.cssClass = "ace-basic";
 
 var dom = require("../lib/dom");
 dom.importCssString(exports.cssText, exports.cssClass);
